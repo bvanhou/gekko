6cb8d3a017f1bc23073156e51b66e1505f4f633c
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-29 11:27:11 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -2,10 +2,6 @@
 // frontend as well as the web server.
 
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
-console.log(process.env.OPENSHIFT_NODEJS_PORT);
-console.log('world');
-console.log(process.env.OPENSHIFT_NODEJS_IP);
-console.log(process.env.NODE_ENV);
 const CONFIG = {
   headless: true,
   api: {
