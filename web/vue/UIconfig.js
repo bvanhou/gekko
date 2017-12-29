52b0b256406ed0481e03c34c5802dc222f4e9675
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-29 18:38:05 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -3,23 +3,25 @@
 // process.env.IP   || process.env.OPENSHIFT_NODEJS_IP 
 // process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
-exports default {
-  headless: false,
-    api: {
-      host: '0.0.0.0',
-      port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-      timeout: 120000 // 2 minutes
-    },
-    ui: {
-      ssl: false,
-      host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP,
-      port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-      path: '/'
-    },
-    adapter: 'sqlite'
-};
+define(function (require, exports, module) {
 
+   module.exports =  {
+    headless: false,
+      api: {
+        host: '0.0.0.0',
+        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        timeout: 120000 // 2 minutes
+      },
+      ui: {
+        ssl: false,
+        host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP,
+        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        path: '/'
+      },
+      adapter: 'sqlite'
+  };
 
+});
 
 // if(typeof window === 'undefined')
 //   module.exports = CONFIG;
