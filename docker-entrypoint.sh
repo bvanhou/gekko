8290b94eac214be7c3b77d592c743e71d6bdc5a3
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-30 01:06:40 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+IP=hostname -i
+
 sed -i 's/127.0.0.1/0.0.0.0/g' ./web/vue/UIconfig.js
-sed -i 's/localhost/gekko-crypto.192.168.64.2.nip.io/g' ./web/vue/UIconfig.js
+sed -i 's/localhost/'${IP}'/g' ./web/vue/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' ./web/vue/UIconfig.js
 exec "$@"
