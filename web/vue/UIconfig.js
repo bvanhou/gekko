93e74b32bdbfe838919747151a11133f86228069
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-29 22:58:04 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -7,13 +7,13 @@ const CONFIG = {
   headless: false,
   api: {
     host: '127.0.0.1',
-    port: 3000,
+    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
-    port: 3000,
+    host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.2',
+    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     path: '/'
   },
   adapter: 'sqlite'
