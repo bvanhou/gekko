5ce121f7f2bacca90124a06b97fdae964584ead3
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-30 16:34:18 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -20,7 +20,35 @@ const CONFIG = {
 }
 
 if(typeof window === 'undefined'){
-  module.exports = CONFIG;
+  module.exports = {
+    headless: true,
+    api: {
+      host: '127.0.0.1',
+      port: 3000,
+      timeout: 120000 // 2 minutes
+    },
+    ui: {
+      ssl: false,
+      host: 'localhost',
+      port: 3000,
+      path: '/'
+    },
+    adapter: 'sqlite'
+  };
 }else{
-  window.CONFIG = CONFIG;
+  window.CONFIG = {
+    headless: true,
+    api: {
+      host: '0.0.0.0',
+      port: '',
+      timeout: 120000 // 2 minutes
+    },
+    ui: {
+      ssl: false,
+      host: 'localhost',
+      port: 3000,
+      path: '/'
+    },
+    adapter: 'sqlite'
+  };
 }
