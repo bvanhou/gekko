32866bf0ad7f6956572915e0c73b3c938b5f3685
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-30 16:41:31 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -2,22 +2,6 @@
 // frontend as well as the web server.
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
-// const CONFIG = {
-//   headless: true,
-//   api: {
-//     host: '127.0.0.1',
-//     port: 3000,
-//     timeout: 120000 // 2 minutes
-//   },
-//   ui: {
-//     ssl: false,
-//     host: 'localhost',
-//     port: 3000,
-//     path: '/'
-//   },
-//   adapter: 'sqlite'
-// }
-
 if(typeof window === 'undefined'){
   module.exports = {
     headless: true,
