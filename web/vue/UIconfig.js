af9c7425cf63ebdf6320a31cb103110dc57bf474
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-30 16:36:50 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -1,23 +1,22 @@
 // This config is used in both the
 // frontend as well as the web server.
-// process.env.IP   || process.env.OPENSHIFT_NODEJS_IP 
-// process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
-const CONFIG = {
-  headless: true,
-  api: {
-    host: '127.0.0.1',
-    port: 3000,
-    timeout: 120000 // 2 minutes
-  },
-  ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
-    path: '/'
-  },
-  adapter: 'sqlite'
-}
+
+// const CONFIG = {
+//   headless: true,
+//   api: {
+//     host: '127.0.0.1',
+//     port: 3000,
+//     timeout: 120000 // 2 minutes
+//   },
+//   ui: {
+//     ssl: false,
+//     host: 'localhost',
+//     port: 3000,
+//     path: '/'
+//   },
+//   adapter: 'sqlite'
+// }
 
 if(typeof window === 'undefined'){
   module.exports = {
