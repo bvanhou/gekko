7afedeb4b7900d3dda0e7de223425a2d7ba477f7
Benjamin Van Houten <bvanhou@gmail.com>
2017-12-29 19:05:59 -0500

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -3,27 +3,28 @@
 // process.env.IP   || process.env.OPENSHIFT_NODEJS_IP 
 // process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
-define(function (require, exports, module) {
+ module.exports =  {
+  headless: false,
+    api: {
+      host: '0.0.0.0',
+      port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+      timeout: 120000 // 2 minutes
+    },
+    ui: {
+      ssl: false,
+      host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP,
+      port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+      path: '/'
+    },
+    adapter: 'sqlite'
+};
 
-   module.exports =  {
-    headless: false,
-      api: {
-        host: '0.0.0.0',
-        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        timeout: 120000 // 2 minutes
-      },
-      ui: {
-        ssl: false,
-        host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP,
-        port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        path: '/'
-      },
-      adapter: 'sqlite'
-  };
+if(typeof window === 'undefined')
+  console.log('undefined window');
+  module.exports = CONFIG;
+else{
+  console.log('defined window');
+  window.CONFIG = CONFIG;
+  
+}
 
-});
-
-// if(typeof window === 'undefined')
-//   module.exports = CONFIG;
-// else
-//   window.CONFIG = CONFIG;
